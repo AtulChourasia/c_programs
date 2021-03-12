63d01556e2132dd6e0ffcc4536a06d19d8bbe6a1
AtulChourasia <67797718+AtulChourasia@users.noreply.github.com>
2021-03-12 14:56:03 +0530

    Create pattern7.c

---
+++ patterns/pattern7.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+  int n,i,j;
+    printf("Enter the Size of pattern : ");
+  scanf("%d",&n);
+  for(i=0;i<n;i++){
+  for(j=0;j<=n;j++){
+    if(j<=i){
+    printf(" ");
+      }else{
+    printf("* ");      
+      }
+  }
+    printf("\n");
+    
+  }
+    return 0;
+}
+  
+//OUTPUT
+
+/*
+Enter the Size of pattern : 5
+ * * * * * 
+  * * * * 
+   * * * 
+    * * 
+     * 
+*/
