4874705baeaff4b30d8911e81b3d2b6ce4223170
AtulChourasia <67797718+AtulChourasia@users.noreply.github.com>
2021-03-12 14:34:31 +0530

    Create pattern4.c

---
+++ patterns/pattern4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+  int n,i,j;
+    printf("Enter the Size of pattern : ");
+  scanf("%d",&n);
+  for(i=0;i<n;i++){
+  for(j=0;j<=n;j++){
+    if(j>n-(i+1)){
+    printf("*");
+      }else{
+    printf(" ");
+      
+      }
+  }
+    printf("\n");
+    
+  }
+    return 0;
+}
+  
+
+//OUTPUT
+
+/*
+
+Enter the Size of pattern : 5
+     *
+    **
+   ***
+  ****
+ *****
+
+*/
