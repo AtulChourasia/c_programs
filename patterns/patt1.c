45a9cc0963a0442b15ea5d35d9d09991c78b1d68
AtulChourasia <67797718+AtulChourasia@users.noreply.github.com>
2021-03-12 14:04:05 +0530

    pattern1

---
+++ patterns/patt1.c
@@ -0,0 +1,28 @@
+
+
+#include <stdio.h>
+
+int main() {
+  int n,i,j;
+    printf("Enter the Size of pattern : ");
+  scanf("%d",&n);
+  for(i=0;i<n;i++){
+  for(j=0;j<n;j++){
+    printf("* ");
+  }
+    printf("\n");
+    
+  }
+    return 0;
+}
+
+// OUTPUT
+
+/*
+Enter the Size of pattern : 5
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * *
+*/
