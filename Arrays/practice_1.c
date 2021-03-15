c441ffd701e7be82313fa2ff41586f66f9be445f
AtulChourasia <67797718+AtulChourasia@users.noreply.github.com>
2021-03-15 06:50:47 +0530

    Create practice_1.c

---
+++ Arrays/practice_1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+  int a[5];
+  int i;
+  for(i=0;i<5;i++){
+    scanf("%d",&a[i]);
+  }
+  for(i=0;i<5;i++){
+    printf("\n%d",a[i]);
+    }
+  return 0;
+}
+  
+
+//output
+
+/*
+
+1 2 3 4 5 
+
+1
+2
+3
+4
+5
+
+*/
